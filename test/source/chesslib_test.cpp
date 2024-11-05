39c718ac31f66fc4f877e5f2270b50145cbc47cb
Bogdan Burlacu <bogdan.burlacu@pm.me>
2024-11-05 17:02:03 +0100

    update unit tests

--- test/source/chesslib_test.cpp
+++ test/source/chesslib_test.cpp
@@ -15,7 +15,7 @@ auto print(board const& b, auto&& pred, char const* marker, fmt::color color) {
     auto constexpr n = coord::nrow;
     for (auto i = n - 1; i >= 0; --i) {
         for (auto j = 0; j < n; ++j) {
-            auto const s = coord::square(i, j);
+            auto const s = coord::square_index(i, j);
             auto const p = me::enum_integer(pieces[s]);
             auto const* c = piece_symbols[colors[s] == color::white ? 0 : 1][p];
             auto fg = fmt::color::white;
@@ -32,7 +32,7 @@ auto print(board const& b, auto&& pred, char const* marker, fmt::color color) {
     }
 }
 
-auto format_moves(board const& board, move_list const& moves) {
+auto format_moves(board const& board, move_list const& moves) -> std::vector<std::string> {
     // group by target square since this changes the notation => Nec3, Nbc3
     ankerl::unordered_dense::map<std::pair<u8, u8>, std::vector<move>> map;
 
@@ -54,6 +54,7 @@ auto format_moves(board const& board, move_list const& moves) {
         auto tgt = m.target_square;
 
         auto [p, c] = board[src];
+        ASSERT(p != piece::none);
 
         std::string str;
 
@@ -80,7 +81,7 @@ auto format_moves(board const& board, move_list const& moves) {
         } else {
             str.push_back(piece_letters[0][me::enum_integer(p)]);
             auto const& moves_to_tgt = map.find({static_cast<u8>(p), tgt})->second;
-            if (moves_to_tgt.size() == 2) {
+            if (moves_to_tgt.size() > 1) {
                 auto a = moves_to_tgt.front();
                 auto b = moves_to_tgt.back();
                 str.push_back(coord::file(a.source_square) == coord::file(b.source_square)
@@ -97,6 +98,10 @@ auto format_moves(board const& board, move_list const& moves) {
 
     return out;
 }
+
+
+template<piece P>
+static constexpr bool is_sliding_v = ((P == piece::bishop) || (P == piece::rook) || (P == piece::queen));
 } // namespace helpers
 
 TEST_CASE("piece values", "[library]")
@@ -107,6 +112,16 @@ TEST_CASE("piece values", "[library]")
         constexpr auto p = val();
         fmt::print("{}: {}\n", me::enum_name(p), me::enum_integer(p));
     });
+}
+
+TEST_CASE("sliding", "[library]")
+{
+    ASSERT(helpers::is_sliding_v<piece::pawn>   == false);
+    ASSERT(helpers::is_sliding_v<piece::knight> == false);
+    ASSERT(helpers::is_sliding_v<piece::bishop> == true);
+    ASSERT(helpers::is_sliding_v<piece::rook>   == true);
+    ASSERT(helpers::is_sliding_v<piece::queen>  == true);
+    ASSERT(helpers::is_sliding_v<piece::king>   == false);
 }
 
 TEST_CASE("move generator", "[library]")
@@ -200,24 +215,27 @@ TEST_CASE("attacked squares", "[library]")
     auto attacked = [](auto& b, auto s) { return b.is_attacked(s, b.side()); };
 
     constexpr std::array test_cases = {
-        "3rr3/1p1kbQ2/p1p1n3/q2p1Bp1/3P4/2N1P1P1/PP4P1/4RRK1 w",
-        "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w",
-        "8/7p/8/7k/2K1N3/6Q1/5B2/8 w",
-        "8/8/8/8/3K4/8/8/8 w",
-        "5rr1/8/8/8/8/8/q3PP2/R3K2R w",
-        "5rr1/8/8/8/8/5p2/q3PP2/R3K2R w",
-        "8/8/2r3n1/8/4B3/5N2/2R3P1/8 w",
-        "8/8/2r3n1/8/4B3/5n2/2q3p1/8 w",
-        "8/8/2r3n1/8/4B3/5n2/2q3p1/8 b",
-        "5R2/k5r1/P2p4/1K1Np3/1P2P1p1/8/8/1r6 w",
-        "5R2/k5r1/P2p4/1K1Np3/1P2P1p1/8/8/1r6 b",
+        // "3rr3/1p1kbQ2/p1p1n3/q2p1Bp1/3P4/2N1P1P1/PP4P1/4RRK1 w",
+        // "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w",
+        // "8/7p/8/7k/2K1N3/6Q1/5B2/8 w",
+        // "8/8/8/8/3K4/8/8/8 w",
+        // "5rr1/8/8/8/8/8/q3PP2/R3K2R w",
+        // "5rr1/8/8/8/8/5p2/q3PP2/R3K2R w",
+        // "8/8/2r3n1/8/4B3/5N2/2R3P1/8 w",
+        // "8/8/2r3n1/8/4B3/5n2/2q3p1/8 w",
+        // "8/8/2r3n1/8/4B3/5n2/2q3p1/8 b",
+        // "5R2/k5r1/P2p4/1K1Np3/1P2P1p1/8/8/1r6 w",
+        // "5R2/k5r1/P2p4/1K1Np3/1P2P1p1/8/8/1r6 b",
+        "8/4k3/8/8/8/8/r6r/4K3 w - - 0 1",
+        "8/4k3/8/8/8/8/r6r/4K3 b - - 0 1"
     };
 
     for (auto const* fen : test_cases) {
         fmt::print("{}\n", fen);
         chesslib::board board{fen};
-        helpers::print(board, attacked, "▢", board.side() == side::white ? fmt::color::red : fmt::color::blue);
+        helpers::print(board, attacked, "▢", board.side() == side_to_move::white ? fmt::color::red : fmt::color::blue);
         fmt::print("\n");
+        fmt::print("in check: {}\n", board.is_king_in_check());
     }
 }
 
@@ -228,6 +246,24 @@ TEST_CASE("parse en-passant", "[library]")
     board.print();
 }
 
+TEST_CASE("king in check", "[library]")
+{
+    constexpr std::array test_cases = {
+        "8/4k3/8/8/8/8/r6r/4K3 w - - 0 1",
+        "8/4k3/8/8/8/8/r6r/4K3 b - - 0 1"
+        // "8/8/4k3/8/4p3/3p4/B7/7K b - - 0 1",
+        // "8/4k3/8/8/8/8/r6r/R3K2R w Q - 0 1"
+    };
+
+    for (auto const* fen : test_cases) {
+        fmt::print("fen: {}\n", fen);
+        board b{fen};
+        b.print();
+        fmt::print("king positions - white: {}, black: {}\n", me::enum_name(b.state().white_king), me::enum_name(b.state().black_king));
+        fmt::print("in check: {}\n", b.is_king_in_check());
+    }
+}
+
 TEST_CASE("rampart", "[library]")
 {
     using json = nlohmann::json;
@@ -235,37 +271,76 @@ TEST_CASE("rampart", "[library]")
     auto check_case = [&](auto t) {
         auto const& s = t["start"];
         auto const fen = s["fen"].template get<std::string>();
-        auto const desc = s["description"].template get<std::string>();
-        fmt::print("fen: {}\n", fen);
-        fmt::print("description: {}\n", desc);
-
         auto const& e = t["expected"];
         std::vector<std::string> expected_moves;
 
-        for (auto const& x : e) {
-            auto m = x["move"].template get<std::string>();
-            if (m.back() == '+') {
-                m.pop_back();
+        if (!e.empty()) {
+            for (auto const& x : e) {
+                auto m = x["move"].template get<std::string>();
+                if (m.back() == '+' || m.back() == '#') {
+                    m.pop_back();
+                }
+                expected_moves.push_back(m);
             }
-            expected_moves.push_back(m);
         }
         std::ranges::sort(expected_moves);
-        fmt::print("expected moves: {}\n", expected_moves);
-
         board b{fen};
+
+
         move_list moves;
         move_generator gen{b};
         gen.moves(moves);
+
+        std::erase_if(moves, [&](auto const& m) {
+            auto tmp = b;
+
+            auto [p, c] = b[m.target_square];
+            board_state const s = b.make_move(m);
+            auto ret = b.is_king_in_check();
+            b.unmake_move(m, s);
+            if (m.capture) {
+                b.pieces[m.target_square] = p;
+                b.colors[m.target_square] = c;
+            }
+
+            ASSERT(b.state() == tmp.state());
+            ASSERT(b.pieces == tmp.pieces);
+            ASSERT(b.colors == tmp.colors);
+
+            return ret;
+        });
+
         auto computed_moves = helpers::format_moves(b, moves);
         std::ranges::sort(computed_moves);
         auto color = expected_moves == computed_moves ? fmt::color::green : fmt::color::red;
-        fmt::print(fmt::fg(color), "computed moves: {}\n\n", computed_moves);
-        REQUIRE(expected_moves == computed_moves);
+        if (expected_moves != computed_moves) {
+            fmt::print("fen: {}\n", fen);
+            if (auto it = s.find("description"); it != s.end()) {
+                fmt::print("description: {}\n", it->template get<std::string>());
+            }
+            b.print(); // print the board to see if the fen is parsed correctly
+            fmt::print("expected moves: {} {}\n", expected_moves, expected_moves.size());
+            fmt::print(fmt::fg(color), "computed moves: {} {}\n\n", computed_moves, computed_moves.size());
+            fmt::print("\n");
+            REQUIRE(expected_moves == computed_moves);
+        }
     };
 
     // test cases involving castling
     SECTION("castling") {
         std::ifstream f("./test/testcases/castling.json");
+        json data = json::parse(f);
+        std::ranges::for_each(data["testCases"], check_case);
+    }
+
+    SECTION("checkmates") {
+        std::ifstream f("./test/testcases/checkmates.json");
+        json data = json::parse(f);
+        std::ranges::for_each(data["testCases"], check_case);
+    }
+
+    SECTION("famous") {
+        std::ifstream f("./test/testcases/famous.json");
         json data = json::parse(f);
         std::ranges::for_each(data["testCases"], check_case);
     }
@@ -278,6 +353,12 @@ TEST_CASE("rampart", "[library]")
 
     SECTION("promotions") {
         std::ifstream f("./test/testcases/promotions.json");
+        json data = json::parse(f);
+        std::ranges::for_each(data["testCases"], check_case);
+    }
+
+    SECTION("stalemates") {
+        std::ifstream f("./test/testcases/stalemates.json");
         json data = json::parse(f);
         std::ranges::for_each(data["testCases"], check_case);
     }
