b76df35fe861113f98337665a85ac588cdc32bba
Bogdan Burlacu <bogdan.burlacu@pm.me>
2024-11-05 17:01:57 +0100

    fix bug in fen parser

--- source/util/fen.cpp
+++ source/util/fen.cpp
@@ -7,35 +7,35 @@
 namespace chesslib {
     auto fen_parser::parse(std::string_view fen) -> board {
         board b;
-        u8 rank = 0;
-        u8 sq   = 0;
-
-        using namespace me::bitwise_operators;
+        std::ranges::fill(b.pieces, piece::none);
+        std::ranges::fill(b.colors, color::none);
+        auto& state = b.state();
 
         auto n = 0;
         for (auto field : std::ranges::views::split(fen, ' ')) {
             switch(n++) {
                 case fen_record::piece_placement: {
                     // parse position
-                    auto row = static_cast<u32>(square::a8);
+                    auto row = static_cast<i32>(square::a8);
                     for (auto line : std::ranges::views::split(field, '/')) {
-                        auto sq = row;
+                        auto sq = row-1;
                         for (auto c : line) {
                             if (std::isdigit(c) != 0) {
-                                ASSERT(c-'0' <= 8, "fen digit cannot exceed 8");
-                                auto s = sq;
-                                for (; s < sq + c - '0'; ++s) {
-                                    b.pieces[s] = piece::none;
-                                    b.colors[s] = color::none;
-                                }
-                                sq = s;
-                                continue;
+                                sq += c-'0';
                             }
-                            if (std::isalpha(c) != 0) {
-                                auto col   = std::isupper(c) != 0 ? color::white : color::black;
-                                b.pieces[sq] = char2piece(static_cast<char>(std::tolower(c)));
+                            else if (std::isalpha(c) != 0) {
+                                sq += 1;
+
+                                auto col     = std::isupper(c) != 0 ? color::white : color::black;
+                                auto pic     = char2piece(static_cast<char>(std::tolower(c)));
+                                b.pieces[sq] = pic;
                                 b.colors[sq] = col;
-                                ++sq;
+
+                                // keep track of the king position
+                                if (pic == piece::king) {
+                                    auto& k = col == color::white ? state.white_king : state.black_king;
+                                    k = static_cast<square>(sq);
+                                }
                             }
                         }
                         row -= coord::ncol;
@@ -44,51 +44,51 @@ namespace chesslib {
                 }
                 case fen_record::active_color: {
                     // parsing side to move
-                    b.side_ = field.front() == 'w' ? side::white : side::black;
+                    b.side() = field.front() == 'w' ? side_to_move::white : side_to_move::black;
                     break;
                 }
                 case fen_record::castling_availability: {
                     // parsing castling rights
-                    u8 castling_rights = 0;
+                    u8 castling = 0;
                     for (auto c : field) {
                         switch(c) {
                             case 'K':
-                                castling_rights |= me::enum_integer(castle::wk);
+                                castling |= me::enum_integer(castling_rights::wk);
                                 break;
                             case 'Q':
-                                castling_rights |= me::enum_integer(castle::wq);
+                                castling |= me::enum_integer(castling_rights::wq);
                                 break;
                             case 'k':
-                                castling_rights |= me::enum_integer(castle::bk);
+                                castling |= me::enum_integer(castling_rights::bk);
                                 break;
                             case 'q':
-                                castling_rights |= me::enum_integer(castle::bq);
+                                castling |= me::enum_integer(castling_rights::bq);
                                 break;
                             default:
                                 break;
                         }
                     }
-                    b.castle_ = static_cast<castle>(castling_rights);
+                    state.castling = static_cast<castling_rights>(castling);
                     break;
                 }
                 case fen_record::en_passant_target: {
                     // parsing en-passant
                     auto res = me::enum_cast<square>(std::string_view{field.begin(), field.end()});
                     if (res) {
-                        b.enpassant_ = res.value();
+                        state.enpassant = res.value();
                     }
                     break;
                 }
                 case fen_record::halfmove_clock: {
-                    // parsing half-move clock: the number of halfmoves since the
+                    // parsing half-move clock: the number of half-moves since the
                     // last capture or pawn advance (used for the 50-move rule)
-                    b.ply_ = std::stoi(std::string{field.begin(), field.end()});
+                    state.ply = std::stoi(std::string{field.begin(), field.end()});
                     break;
 
                 }
                 case fen_record::fullmove_number: {
-                    // parsing full-move numbe: the number of full moves
-                    b.movecount_ = std::stoi(std::string{field.begin(), field.end()});
+                    // parsing full-move number: the number of full moves
+                    state.count = std::stoi(std::string{field.begin(), field.end()});
                     break;
                 }
                 default: {
@@ -97,7 +97,6 @@ namespace chesslib {
                 }
             }
         }
-
         return b;
     }
 }  // namespace chesslib
