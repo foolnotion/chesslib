6926c2c8d7d84cc5eeaf40c0263dd1da6069f38e
Bogdan Burlacu <bogdan.burlacu@pm.me>
2026-04-07 11:26:28 +0300

    fix install config: add find_dependency for all public deps

--- cmake/install-config.cmake
+++ cmake/install-config.cmake
@@ -1,1 +1,11 @@
+include(CMakeFindDependencyMacro)
+
+find_dependency(fmt CONFIG)
+find_dependency(libassert CONFIG)
+find_dependency(magic_enum CONFIG)
+find_dependency(mdspan CONFIG)
+find_dependency(small_vector CONFIG)
+find_dependency(tl-expected CONFIG)
+find_dependency(unordered_dense CONFIG)
+
 include("${CMAKE_CURRENT_LIST_DIR}/chesslibTargets.cmake")
