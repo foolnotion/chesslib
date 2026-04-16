b0d19fa9cbed022dae1c2f0446aaac42193d17b2
Bogdan Burlacu <bogdan.burlacu@pm.me>
2026-04-16 22:42:05 +0300

    fix install-config for static builds

--- cmake/install-config.cmake
+++ cmake/install-config.cmake
@@ -1,5 +1,12 @@
 include(CMakeFindDependencyMacro)
 
+# needed for static builds — PRIVATE deps are $<LINK_ONLY:...> for static archives
+if(NOT chesslib_SHARED_LIBS)  # or check via a generated variable
+    find_dependency(fmt CONFIG)
+    find_dependency(libassert CONFIG)
+    find_dependency(mdspan CONFIG)
+endif()
+
 find_dependency(magic_enum CONFIG)
 find_dependency(small_vector CONFIG)
 find_dependency(tl-expected CONFIG)
